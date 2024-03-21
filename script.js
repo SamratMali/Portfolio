dce831db1c238271bba18987499c494b71d0f160
SamratMali <142326294+SamratMali@users.noreply.github.com>
2024-03-21 10:48:07 +0530

    update mode

--- script.js
+++ script.js
@@ -9,6 +9,25 @@ icon.onclick = function () {
   }
 };
 
+// image change
+document.getElementById("icon").addEventListener("click", function () {
+  var currentImage = document.getElementById("icon").src;
+  var currentText = document.getElementById("icon").innerHTML;
+
+  if (
+    currentImage.endsWith(
+      "https://cdn.iconscout.com/icon/free/png-256/free-night-mode-2-475098.png"
+    )
+  ) {
+    document.getElementById("icon").src = "sun.png";
+    document.getElementById("icon").innerHTML = "Dark Mode";
+  } else {
+    document.getElementById("icon").src =
+      "https://cdn.iconscout.com/icon/free/png-256/free-night-mode-2-475098.png";
+    document.getElementById("icon").innerHTML = "Light Mode";
+  }
+});
+
 // button click sound
 
 const clickSoundUrl = "./click.mp3"; // Adjust this to your sound file location
